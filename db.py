83eed5abf2e73a63ddc6c45248ab4cc287e816be
Ishanu Krishna Srivastava <ishanukrishanaf9@gmail.com>
2025-08-08 14:49:45 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,9 +2,12 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 
+from psycopg2.extras import RealDictCursor
+
 load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        dsn=os.getenv("DATABASE_URI")
+        dsn=os.getenv("DATABASE_URI"),cursor_factory=RealDictCursor
+     
     )
